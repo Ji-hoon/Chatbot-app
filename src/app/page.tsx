e950bd072677a94834f4e6d75a680ea124a8cfa7
[SW7] 김지훈 <lanslot84@gmail.com>
2023-12-07 13:47:18 +0900

    FEAT : api 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,10 @@ import { useTheme } from "next-themes"
 export default function Home() {
     const { theme, setTheme } = useTheme();
     console.log("theme", theme);
+    // 테마는 변경이 되는데, 클래스네임이 반영이 안되네...
     return (
         <>
-        <div className="text-blue-800 dark:bg-primary">Page</div>
+        <div className="bg-white dark:bg-slate-800 dark:bg-primary">Page</div>
         <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
           {theme === "dark" ? "🌞 light모드로 변환" : "🌚 dark모드로 변환"}
         </div>
