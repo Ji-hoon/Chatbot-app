bce44b9619f7aa9b3dddf2ed5d3a0a23e69b701c
[SW7] 김지훈 <lanslot84@gmail.com>
2023-12-07 13:02:13 +0900

    FEAT : useTheme, tailwind 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-export default function Page() {
+'use client'
+
+import { useTheme } from "next-themes"
+
+export default function Home() {
+    const { theme, setTheme } = useTheme();
+    console.log("theme", theme);
     return (
-        <div>Page</div>
+        <>
+        <div className="text-blue-800 dark:bg-primary">Page</div>
+        <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          {theme === "dark" ? "🌞 light모드로 변환" : "🌚 dark모드로 변환"}
+        </div>
+      </>
     )
 }
